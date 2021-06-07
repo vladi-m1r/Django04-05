cc930cd6a54f2f144ca8c98585dc858a5fc9b0fe
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-06-06 23:24:43 -0500

    Agregue home a urls en miProyecto

--- miProyecto/urls.py
+++ miProyecto/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from inicio.views import viewHome
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', viewHome, name="Home"),
 ]
