d09fc1c15266a8852e2c2776c255f4664f514ca4
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-07-14 00:58:11 -0500

    Agregue una vista para la app personas

--- personas/views.py
+++ personas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from personas import forms
+from .forms import RawPersonaForm
 
 # Create your views here.
+def personasAnotherCretateView(request):
+    form = RawPersonaForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, 'personas/personasCreate.html', context)
