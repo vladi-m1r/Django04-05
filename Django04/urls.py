c2072cf05c7fb2713dece71be2388a97b65eef00
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-07-14 00:58:27 -0500

    Se agrego las url de personas al proyecto

--- Django04/urls.py
+++ Django04/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
+    path('personas/', include('personas.urls')),
     path('admin/', admin.site.urls),
 ]
