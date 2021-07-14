003d87b10a5cba53bd47ac790feb3229c9edaa33
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-07-14 17:21:37 -0500

    url para detailView

--- personas/urls.py
+++ personas/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from personas.views import PersonaListView, home, personaCreateView, personasAnotherCretateView, personasDeleteView, personasListView, personasShowObject
+from personas.views import PersonaDetailView, PersonaListView, home, personaCreateView, personasAnotherCretateView, personasDeleteView, personasListView, personasShowObject
 
 
 app_name = 'personas'
 urlpatterns = [
     path('', PersonaListView.as_view(), name= 'persona-list'),
+    path('<int:pk>', PersonaDetailView.as_view(), name= 'persona-detail'),
     path('anotherAdd', personasAnotherCretateView, name="OtroAgregarPersonas"),
     path('addPersona', personaCreateView, name="agregarPersona"),
-    path('<int:myID>', personasShowObject, name='browsing'),
     path('<int:myID>/delete', personasDeleteView, name='deleting'),
     path('lista/', personasListView, name='listing'),
 ]
