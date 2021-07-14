deea6a694a245a90857c63462f91db3f31bf96b5
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-07-14 01:30:07 -0500

    Uso de widgets en RawForm de Personas

--- personas/forms.py
+++ personas/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 
 class RawPersonaForm(forms.Form):
-    nombre = forms.CharField(label = "Tu nombre")
+    nombre = forms.CharField(
+        widget= forms.Textarea(
+            attrs={
+                'placeholder': "Solo tu nombre, porfavor",
+                'id': 'nombreID',
+                'class': 'special',
+                'cols': '10',
+            }
+        )
+    )
     apellidos = forms.CharField()
     edad = forms.IntegerField(initial = 20)
     donador = forms.BooleanField()
